a6060a53565922be06ca8056fdbe3d9884bbf541
Danny <danny@dannydabbles.com>
2019-11-15 20:15:55 -0800

    Removing AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -118,11 +118,6 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: StreamBuilder<List<Widget>>(
         stream: loadData(),
         builder: (context, snapshot) {
@@ -142,7 +137,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       backgroundColor: Colors.blue,
                       flexibleSpace: FlexibleSpaceBar(
                           centerTitle: true,
-                          title: Text('My Library',
+                          title: Text(widget.title,
                               style: TextStyle(
                                 color: Colors.white,
                                 fontSize: 16.0,
