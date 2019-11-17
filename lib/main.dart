026bfd4ee28702f4633132a02b0694ea19aa9334
Danny <danny@dannydabbles.com>
2019-11-17 12:14:01 -0800

    Add minimal AppBar for notification visibility

--- lib/main.dart
+++ lib/main.dart
@@ -197,6 +197,10 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(0),
+        child: AppBar(),
+      ),
       backgroundColor: Colors.black12,
       body: StreamBuilder<List<Widget>>(
         stream: loadData(),
